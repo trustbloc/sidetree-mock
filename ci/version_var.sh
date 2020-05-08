6b7c3853ec17be8c8ed70ebb4b815396b193ea01
Troy Ronda <troy.ronda@securekey.com>
2020-05-08 18:21:44 -0400

    ci: publish snapshot images to snapshot repo
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

--- ci/version_var.sh
+++ ci/version_var.sh
@@ -9,14 +9,23 @@
 BASE_VERSION=0.1.4
 IS_RELEASE=false
 
+# Project Parameters
+SOURCE_REPO=sidetree-mock
+BASE_PKG_NAME=sidetree-mock
+RELEASE_REPO=docker.pkg.github.com/trustbloc/${SOURCE_REPO}
+SNAPSHOT_REPO=docker.pkg.github.com/trustbloc-cicd/snapshot
+
 ARCH=$(go env GOARCH)
 
-if [ $IS_RELEASE == false ]
+if [ ${IS_RELEASE} = false ]
 then
   EXTRA_VERSION=snapshot-$(git rev-parse --short=7 HEAD)
-  PROJECT_VERSION=$BASE_VERSION-$EXTRA_VERSION
+  PROJECT_VERSION=${BASE_VERSION}-${EXTRA_VERSION}
+  PROJECT_PKG_REPO=${SNAPSHOT_REPO}
 else
-  PROJECT_VERSION=$BASE_VERSION
+  PROJECT_VERSION=${BASE_VERSION}
+  PROJECT_PKG_REPO=${RELEASE_REPO}
 fi
 
 export SIDETREE_MOCK_TAG=$ARCH-$PROJECT_VERSION
+export SIDETREE_MOCK_PKG=${PROJECT_PKG_REPO}/${BASE_PKG_NAME}
