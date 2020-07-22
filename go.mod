099f6b2967502be7985837c188694cebd03583c1
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2020-07-22 16:13:44 -0400

    chore: update sidetree core (get protocol by txn number)
    Update sidetree-core:
    - added function to retrieve protocol by transaction (block) number
    
    Add  implementation of this function to protocol client.
    
    Closes #231
    
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.4.0
-	github.com/trustbloc/sidetree-core-go v0.1.4-0.20200720163951-97e8fe7645c3
+	github.com/trustbloc/sidetree-core-go v0.1.4-0.20200722185334-8fdfdf4b36da
 )
 
 go 1.13
--- pkg/mocks/protocol.go
+++ pkg/mocks/protocol.go
@@ -7,6 +7,8 @@ SPDX-License-Identifier: Apache-2.0
 package mocks
 
 import (
+	"fmt"
+
 	"github.com/trustbloc/sidetree-core-go/pkg/api/protocol"
 )
 
@@ -14,29 +16,42 @@ const maxBatchFileSize = 2000000 // in bytes
 
 // MockProtocolClient mocks protocol for testing purposes.
 type MockProtocolClient struct {
-	Protocol protocol.Protocol
+	protocols []protocol.Protocol
 }
 
 // NewMockProtocolClient creates mocks protocol client
 func NewMockProtocolClient() *MockProtocolClient {
-
-	return &MockProtocolClient{
-		Protocol: protocol.Protocol{
-			StartingBlockChainTime:       0,
-			HashAlgorithmInMultiHashCode: 18,
-			MaxOperationsPerBatch:        1, // one operation per batch - batch gets cut right away
-			MaxDeltaByteSize:             200000,
-			CompressionAlgorithm:         "GZIP",
-			MaxChunkFileSize:             maxBatchFileSize,
-			MaxMapFileSize:               maxBatchFileSize,
-			MaxAnchorFileSize:            maxBatchFileSize,
-		},
+	latest := protocol.Protocol{
+		StartingBlockChainTime:       0,
+		HashAlgorithmInMultiHashCode: 18,
+		MaxOperationsPerBatch:        1, // one operation per batch - batch gets cut right away
+		MaxDeltaByteSize:             200000,
+		CompressionAlgorithm:         "GZIP",
+		MaxChunkFileSize:             maxBatchFileSize,
+		MaxMapFileSize:               maxBatchFileSize,
+		MaxAnchorFileSize:            maxBatchFileSize,
 	}
+
+	// has to be sorted for mock client to work
+	versions := []protocol.Protocol{latest}
+
+	return &MockProtocolClient{protocols: versions}
 }
 
 // Current mocks getting last protocol version
 func (m *MockProtocolClient) Current() protocol.Protocol {
-	return m.Protocol
+	return m.protocols[len(m.protocols)-1]
+}
+
+// Get mocks getting protocol version based on blockchain(transaction) time
+func (m *MockProtocolClient) Get(transactionTime uint64) (protocol.Protocol, error) {
+	for i := len(m.protocols) - 1; i >= 0; i-- {
+		if transactionTime >= m.protocols[i].StartingBlockChainTime {
+			return m.protocols[i], nil
+		}
+	}
+
+	return protocol.Protocol{}, fmt.Errorf("protocol parameters are not defined for block chain time: %d", transactionTime)
 }
 
 // NewMockProtocolClientProvider creates new mock protocol client provider
